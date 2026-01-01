14863a35c94dea35f677ad7b141bbd3c3b9a2225
StreetArtist <Stephan@thiel-world.de>
2026-01-01 11:12:09 +0100

    min

--- main.js
+++ main.js
@@ -1,50 +1,46 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core');
-const stateMethods = require('./lib/state');
 
 class AiAutopilot extends utils.Adapter {
   constructor(options = {}) {
     super({ ...options, name: 'ai-autopilot' });
-
     this.on('ready', this.onReady.bind(this));
-    this.on('stateChange', this.onStateChange.bind(this));
     this.on('unload', this.onUnload.bind(this));
-
-    this.running = false;
   }
 
   async onReady() {
-    await this.ensureStates();
+    await this.setObjectNotExistsAsync('info.connection', {
+      type: 'state',
+      common: {
+        type: 'boolean',
+        role: 'indicator.connected',
+        read: true,
+        write: false,
+        def: false
+      },
+      native: {}
+    });
+
+    await this.setObjectNotExistsAsync('report.last', {
+      type: 'state',
+      common: {
+        type: 'string',
+        role: 'text',
+        read: true,
+        write: false,
+        def: ''
+      },
+      native: {}
+    });
+
     await this.setStateAsync('info.connection', true, true);
-    this.log.info('ai-autopilot minimal ready');
+    this.log.info('ai-autopilot MINIMAL READY');
   }
 
-  async onUnload(cb) {
+  onUnload(cb) {
     cb();
   }
-
-  async runAnalysis() {
-    const read = async (id) => {
-      if (!id) return null;
-      const s = await this.getForeignStateAsync(id);
-      return s ? s.val : null;
-    };
-
-    const house = await read(this.config.energy.houseConsumption);
-    const pv = await read(this.config.energy.pvPower);
-    const grid = await read(this.config.energy.gridPower);
-
-    const report = [
-      `House: ${house}`,
-      `PV: ${pv}`,
-      `Grid: ${grid}`
-    ].join('\n');
-
-    await this.setStateAsync('report.last', report, true);
-  }
 }
-
-Object.assign(AiAutopilot.prototype, stateMethods());
 
 module.exports = (options) => new AiAutopilot(options);
